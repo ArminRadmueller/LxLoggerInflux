c1d7fb4250ba88e450eb70465d6be714715b0bb1
Armin Radmüller <Armin.Radmueller@live.com>
2023-02-19 17:23:33 +0100

    change log message text

--- src/loxone.ts
+++ src/loxone.ts
@@ -18,8 +18,8 @@ class LxLoggerServer extends EventEmitter {
         super();
         this.Port = Port;
         this.Socket = dgram.createSocket('udp4');
-        this.Socket.on('listening', () => { this.Connected = true; logging.info('udp-socket listening on ' + this.Socket.address().address + ':' + this.Socket.address().port) });
-        this.Socket.on('close', () => { this.Connected = false; logging.info('udp-socket shutdown and connection closed') });
+        this.Socket.on('listening', () => { this.Connected = true; logging.info('listening on ' + this.Socket.address().address + ':' + this.Socket.address().port) });
+        this.Socket.on('close', () => { this.Connected = false; logging.info('socket shutdown and connection closed') });
         this.Socket.on('error', (error:Error) => { this.errorHandler(error) });
         this.Socket.on('message', (message:Buffer, remote:dgram.RemoteInfo) => { this.receive(message,remote) });
         this.Socket.bind(this.Port);
@@ -28,7 +28,7 @@ class LxLoggerServer extends EventEmitter {
     private receive(message:Buffer, remote:dgram.RemoteInfo)
     {
         let messageText:string = message.toString().trim();
-        logging.info('udp-socket received message from ' + remote.address + ':' + remote.port + ' => "' + messageText + '"');
+        logging.info('received message from ' + remote.address + ':' + remote.port + ' => "' + messageText + '"');
         this.emit('receive',messageText);
     }
 
